835fd65609c8c85e88f4d3e67bb98f8aad2ace45
Hadirback <mail.evgeny.filippov@gmail.com>
2019-09-06 02:16:32 +0300

    Complete lesson 2

---
+++ Lesson2_Variable/main.py
@@ -0,0 +1,41 @@
+name = 'Кузя'
+age = 1
+period = 3.2
+is_good = True
+person = None
+person_name = 'Max'
+
+print('Попугайчик')
+print(name)
+print('в')
+print(age)
+print('года')
+print('научился говорить')
+print('свое имя')
+print(name)
+print()
+print('Через')
+print(period)
+print('года')
+print('Он научился говорить')
+print('Сколько ему лет')
+print(age + period)
+
+print(type(person_name))
+print(type(age))
+print(type(period))
+print(type(is_good))
+print(type(person))
+
+birthday_year = '1998'
+print(type(birthday_year))
+
+period_person = 20
+print(type(period_person))
+
+age = int(birthday_year) + period_person
+print(age)
+
+some_str = birthday_year + str(period_person)
+print(some_str)
+
